0a06bbbe3eb0df35c913d0f9cb274cde0d1609fc
youngkwon won <wonyk0302@naver.com>
2023-03-14 14:51:00 +0900

    isolate

--- wwwroot/extensions/HelloWorld.js
+++ wwwroot/extensions/HelloWorld.js
@@ -1,8 +1,12 @@
 // 확장기능 추가 및 아이콘버튼 추가 연습
+//import { BaseExtension } from "./BaseExtension.js";  //기존 버튼그룹에 버튼추가 -잘 안됨
+//import { SummaryPanel } from "./SummaryPanel.js";   //기존 버튼그룹에 버튼추가 -잘 안됨
 
 class HelloWorld extends Autodesk.Viewing.Extension {
   constructor(viewer, options) {
     super(viewer, options);
+    this.button1 = null;
+    this.subToolbar = null;
   }
 
   load() {
@@ -14,23 +18,46 @@ class HelloWorld extends Autodesk.Viewing.Extension {
     console.log("HelloWorld is unloaded");
     return true;
   }
-
-  onToolbarCreated(toolbar) {
-    var viewer = this.viewer;
+  // 별도 버튼 그룹으로 만들기
+  onToolbarCreated() {
+    //var viewer = this.viewer;
+    // SubToolbar
+    this.subToolbar = this.viewer.toolbar.getControl("my-Hello-toolbar");
+    if (!this.subToolbar) {
+      this.subToolbar = new Autodesk.Viewing.UI.ControlGroup(
+        "my-Hello-toolbar"
+      );
+      this.viewer.toolbar.addControl(this.subToolbar);
+    }
 
     var button1 = new Autodesk.Viewing.UI.Button("Hello-button");
-    button1.onClick = function (e) {
+    button1.onClick = () => {
+      //To do
       alert("(주)삼우씨엠건축사사무소"); //기입한 내용 출력 됨---------------------
     };
-    button1.addClass("Hello-button"); //아이콘 추가하기 -> main.css에 추가, index.html에도 이미지 추가
-    button1.setToolTip("Hello"); //마우스를 가리킬때 표시되는 문자
-
-    // SubToolbar
-    this.subToolbar = new Autodesk.Viewing.UI.ControlGroup("my-Hello-toolbar");
+    button1.setToolTip("My Extention"); //마우스를 가리킬때 표시되는 문자
+    button1.addClass("Hello-buttonIcon"); //아이콘 추가하기 -> main.css에 추가
     this.subToolbar.addControl(button1);
-
-    toolbar.addControl(this.subToolbar);
   }
+
+  //기존 버튼그룹에 추가 -잘 안됨
+  // onToolbarCreated() {
+  //   // this._panel = new SummaryPanel(
+  //   //   this,
+  //   //   "model-summary-panel",
+  //   //   "Model Summary"
+  //   // );
+
+  //   this._button = this.createToolbarButton(
+  //     "Hello-button",
+  //     "https://img.icons8.com/material/24/null/hello.png",
+  //     "Hello"
+  //   );
+  //   this._button.onClick = () => {
+  //     // TODO
+  //     alert("(주)삼우씨엠건축사사무소"); //기입한 내용 출력 됨---------------------
+  //   };
+  // }
 }
 
 Autodesk.Viewing.theExtensionManager.registerExtension(
---
+++ wwwroot/extensions/handleselectionextension.js
@@ -0,0 +1,85 @@
+import { BaseExtension } from "./BaseExtension.js";
+
+class HandleSelectionExtension extends BaseExtension {
+  //class HandleSelectionExtension extends Autodesk.Viewing.Extension {
+  constructor(viewer, options) {
+    super(viewer, options);
+    this.viewer = viewer;
+    this.button1 = null;
+    this.subToolbar = null;
+  }
+
+  load() {
+    super.load();
+    console.log("HandleSelectionExtension is loaded");
+
+    return true;
+  }
+
+  unload() {
+    super.unload();
+    console.log("HandleSelectionExtension is unloaded");
+    return true;
+  }
+
+  // HelloWorldjs 에서 만든 버튼 그룹에 추가
+  onToolbarCreated() {
+    //this viewer = viewer;
+    // SubToolbar
+    this.subToolbar = this.viewer.toolbar.getControl("my-Hello-toolbar");
+    if (!this.subToolbar) {
+      this.subToolbar = new Autodesk.Viewing.UI.ControlGroup(
+        "my-Hello-toolbar"
+      );
+      this.viewer.toolbar.addControl(this.subToolbar);
+    }
+
+    this.button1 = new Autodesk.Viewing.UI.Button(
+      "handleselectionextensionButton"
+    );
+
+    // 테스트 1 : 선택한 객체 isolated 함수 만들기----------------------------------------------------------------
+    this.button1.onClick = () => {
+      const dbids = this.viewer.getSelection();
+      this.viewer.isolate(dbids);
+    };
+    // ----------------------------------------------------------------------------------------------------------
+
+    // //테스트 2 : 버튼 클릭하면 실행되는 함수
+    // button1.onClick = () => {
+    //   // alert("(주)삼우씨엠건축사사무소"); //기입한 내용 출력 테스트용 함수---------------------
+
+    //   // 선택한 객체 isolated 함수 만들기-------------------------
+    //   // const dbids = this.viewer.getSelection(); //선택한 항목의 아이디 가져오기 위한 객체 생성
+    //   // this.viewer.clearSelection(); //선택한 객체 강조하기
+    //   // // 선택한 객체를 isolate 시키기 함수
+    //   // if (dbids.length > 0) {
+    //   //   let isolated = []; //객체를 격리하기 위한 배열 만들기
+    //   //   //선택한 객체 속성 가져오기, 속성이름과 외부아이디 추가
+    //   //   //confirm(`Isolate ${props.name} (${props.externalId})?`)는 선택한 항목의 속성을 대화창으로 호출하는 함수
+    //   //   // 확인 창에서 ok 누르면 isolated.push(dbid); 함수 실행됨
+    //   //   for (const dbid of dbids) {
+    //   //     this.viewer.getProperties(dbid, (props) => {
+    //   //       if (confirm(`Isolate ${props.name} (${props.externalId})?`)) {
+    //   //         isolated.push(dbid); //리스트에 추가
+    //   //         this.viewer.isolate(isolated); //isolated 리스트 객체를 isolate 시키기
+    //   //       }
+    //   //     });
+    //   //   }
+    //   // }
+    //   // //선택 항목이 비어있으면, 모든 격리된 항목이 제거됩니다.
+    //   // else {
+    //   //   this.viewer.isolate([]); //격리된 항목을 해제한다(비운다)
+    //   // }
+    // };
+
+    this.button1.setToolTip("My Extention"); //마우스를 가리킬때 표시되는 문자
+    this.button1.addClass("handleselectionextensionIcon"); //아이콘 추가하기 -> main.css에 추가
+    this.subToolbar.addControl(this.button1);
+  }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension(
+  "HandleSelectionExtension",
+  HandleSelectionExtension
+);
